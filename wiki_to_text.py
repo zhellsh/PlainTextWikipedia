e1b349dc0b65e683db88d42beaa78be2333176a5
redacted <121198893+zhellsh@users.noreply.github.com>
2024-02-13 20:03:56 -0600

    Update wiki_to_text.py

--- wiki_to_text.py
+++ wiki_to_text.py
@@ -1,8 +1,6 @@
 from dewiki_functions import *
-
-#wiki_xml_file = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
-wiki_xml_file = 'G:/enwiki-20210401/enwiki-20210401.xml'  # update this
-json_save_dir = 'G:/wiki_plaintext/'
+wiki_xml_file = '../simplewiki.xml'  # update this
+json_save_dir = '../out/wiki.json'
 
 if __name__ == '__main__':
     process_file_text(wiki_xml_file, json_save_dir)
